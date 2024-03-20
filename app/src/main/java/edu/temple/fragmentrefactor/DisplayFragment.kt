3e0ff112fcef696efd0989030ee5806460225f3b
sanHemet <20hhemet@gmail.com>
2024-03-20 15:44:52 -0400

    refactored in displayfragment

--- app/src/main/java/edu/temple/fragmentrefactor/DisplayFragment.kt
+++ app/src/main/java/edu/temple/fragmentrefactor/DisplayFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
 class DisplayFragment : Fragment() {
 
     override fun onCreateView(
@@ -12,6 +16,16 @@ class DisplayFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_display, container, false)
+        return inflater.inflate(R.layout.fragment_display, container, false).apply{
+            findViewById<Button>(R.id.changeBtn).setOnClickListener {
+                val name = findViewById<EditText>(R.id.editNameText).text
+
+                findViewById<TextView>(R.id.textView).text = if (name.isNotBlank()) {
+                    "Hello, $name!"
+                } else {
+                    "Please enter your name!"
+                }
+            }
+        }
     }
 }
